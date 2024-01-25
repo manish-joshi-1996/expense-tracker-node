cdca4ed5feeaf0005a898c047e5301fd81a764cf
manish-joshi-1996 <manish.joshi1996@gmail.com>
2024-01-25 20:48:07 +0530

    dwdwd

--- functions/authRoutes.js
+++ functions/authRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/authController');
 
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the expense endpoint' });
+  });
 router.post('/signup', userController.signUp);
 router.put('/verify-user/:id', userController.verifyOtpAndRegister);
 router.post('/send-password-reset-otp', userController.sendPasswordResetOTP);
