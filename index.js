ffe958474f32bd89baebd07592327c4ccf64ff20
manish-joshi-1996 <manish.joshi1996@gmail.com>
2024-01-25 20:46:31 +0530

    dwdwd

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ db.once('open', () => {
 });
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions', authRoutes);
-app.use('/.netlify/functions/api', expenseRoutes);
+app.use('/', authRoutes);
+app.use('/api', expenseRoutes);
 
 
 app.listen(PORT, () => {
